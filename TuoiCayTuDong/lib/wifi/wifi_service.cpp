f880bda78b7d8af7cfb55f5eeb652bd3b6f602cb
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-03-27 22:25:41 +0700

    [WiFi] Define function connecting to default wifi

--- TuoiCayTuDong/lib/wifi/wifi_service.cpp
+++ TuoiCayTuDong/lib/wifi/wifi_service.cpp
@@ -53,6 +53,8 @@
 ** =============================================== */
 
 
+static void connect_default();
+
 static bool ssid_is_valid(const char* ssid);
 static bool pass_is_valid(const char* pass);
 
@@ -61,6 +63,43 @@ static bool pass_is_valid(const char* pass);
 ** Static function declaration
 **
 ** =============================================== */
+
+
+static void connect_default()
+{
+    if(ssid_is_valid(WIFI_DEFAULT_SSID) == false){
+        LOG_E("[WiFi] SSID is empty");
+        return;
+    }
+
+    if(pass_is_valid(WIFI_DEFAULT_PASS) == false){
+        LOG_E("[WiFi] Password is less than 8 characters");
+        return;
+    }
+
+    LOG_I("[WiFi] Connecting to '%s'", WIFI_DEFAULT_SSID);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_DEFAULT_SSID, WIFI_DEFAULT_PASS);
+
+    for(uint8_t i=0; i<3; ++i)
+    {
+        uint32_t timePoint = millis();
+        while((millis()-timePoint) % WIFI_TIME_CONNECT*2 < WIFI_TIME_CONNECT) {}
+
+        if(WiFi.status() == WL_CONNECTED)
+        {
+            LOG_I("[WiFi] Connected");
+
+            LOG_PRINTF("I (%.3Lf) [WiFi] IP address: ", (long double)millis()/1000);
+            LOG_PRINT(WiFi.localIP());
+            LOG_PRINT(F("\n"));
+
+            return;
+        }
+    }
+
+    LOG_I("[WiFi] fail to connect");
+}
 
 
 static bool ssid_is_valid(const char* ssid)
@@ -99,44 +138,9 @@ void WiFi_init()
 {
     LOG_PRINTF("\n");
     LOG_I("[WiFi] start initing");
-
-    if(ssid_is_valid(WIFI_DEFAULT_SSID) == false)
-    {
-        LOG_E("[WiFi] SSID is empty");
-        LOG_I("[WiFi] end initing\n");
-        return;
-    }
-
-    if(pass_is_valid(WIFI_DEFAULT_PASS) == false)
-    {
-        LOG_E("[WiFi] Password is less than 8 characters");
-        LOG_I("[WiFi] end initing\n");
-        return;
-    }
-
-    LOG_I("[WiFi] Connecting to '%s'", WIFI_DEFAULT_SSID);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_DEFAULT_SSID, WIFI_DEFAULT_PASS);
-
-    for(uint8_t i=0; i<3; ++i)
-    {
-        uint32_t timePoint = millis();
-        while((millis()-timePoint) % WIFI_TIME_CONNECT*2 < WIFI_TIME_CONNECT) {}
-
-        if(WiFi.status() == WL_CONNECTED)
-        {
-            LOG_I("[WiFi] Connected");
-
-            LOG_PRINTF("I (%.3Lf) [WiFi] IP address: ", (long double)millis()/1000);
-            LOG_PRINT(WiFi.localIP());
-            LOG_PRINT(F("\n"));
-
-            LOG_I("[WiFi] end initing\n");
-            return;
-        }
-    }
-
-    LOG_I("[WiFi] fail to connect");
+    
+    connect_default();
+    
     LOG_I("[WiFi] end initing\n");
 }
 
